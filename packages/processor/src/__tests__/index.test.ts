49e94598a3fb757ea5ba0a276358de9070344b53
Erick Tamayo <ericktamayo@gmail.com>
2020-07-07 17:52:42 -0600

    wip

---
+++ packages/processor/src/__tests__/index.test.ts
@@ -0,0 +1,47 @@
+import process from "../";
+
+describe("processor", () => {
+  describe("box-shadow", () => {
+    it("should process box-shadow correctly", async () => {
+      expect(
+        await process(
+          ".test { box-shadow: 5px 10px rgba(0, 0, 0, 0.05) }",
+          "mobile"
+        )
+      ).toEqual({
+        "": {
+          test: {
+            elevation: 5,
+            shadowColor: "rgba(0, 0, 0, 0.05)",
+            shadowOffset: {
+              height: 10,
+              width: 5,
+            },
+            shadowOpacity: 1,
+            shadowRadius: 0,
+          },
+        },
+      });
+
+      expect(
+        await process(
+          ".test { box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06) }",
+          "mobile"
+        )
+      ).toEqual({
+        "": {
+          test: {
+            elevation: 1,
+            shadowColor: "rgba(0, 0, 0, 0.1)",
+            shadowOffset: {
+              width: 0,
+              height: 1,
+            },
+            shadowOpacity: 1,
+            shadowRadius: 3,
+          },
+        },
+      });
+    });
+  });
+});

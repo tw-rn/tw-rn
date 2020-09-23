008087232719d5d079f913f1e207dfec685e2050
Erick Tamayo <ericktamayo@gmail.com>
2020-09-23 10:45:11 -0600

    change

--- packages/playground/pages/index.tsx
+++ packages/playground/pages/index.tsx
@@ -8,7 +8,7 @@ export default function App() {
   const fadeAnim = useRef(new Animated.Value(1)).current;
 
   const fadeIn = () => {
-    // Will change fadeAnim value to 1 in 5 seconds
+    // Will change fadeAnim value to 1 in 5  seconds
     Animated.timing(fadeAnim, {
       toValue: 1,
       duration: 1000,
